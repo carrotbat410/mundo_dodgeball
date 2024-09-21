b7d6edf649d1a53bd07b41efdf9fc72df73a4846
carrotbat410 <carrotbat410@gmail.com>
2024-09-21 20:03:01 +0900

    주석

--- routes/auth.go
+++ routes/auth.go
@@ -6,20 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// AuthRoutes는 /auth 그룹 경로를 설정합니다.
-func AuthRoutes(app *fiber.App) {
-	// /auth 그룹 생성
-	authGroup := app.Group("/auth")
-
-	authGroup.Post("/register", controllers.RegisterUser)
-	authGroup.Post("/login", controllers.Login)
-}
-
-// // 라우트 설정 함수
-// func setupRoutes(app *fiber.App) {
-// 	app.Post("/register", controllers.RegisterUser)
-// }
-
 // api := api.Group("/v1", middleware) 필요시 middleware + c.Next() 사용 가능
 
 // app.Get("/", func(c *fiber.Ctx) error {
@@ -30,7 +16,15 @@ func AuthRoutes(app *fiber.App) {
 // 	return fiber.NewError(782, "Custom Error Message")
 // })
 
-// //* 매개변수
 // app.Get("/sample/:name", func(c *fiber.Ctx) error {
 // 	return c.SendString("name: " + c.Params("name"))
 // })
+
+// AuthRoutes는 /auth 그룹 경로를 설정합니다.
+func AuthRoutes(app *fiber.App) {
+	// /auth 그룹 생성
+	authGroup := app.Group("/auth")
+
+	authGroup.Post("/register", controllers.RegisterUser)
+	authGroup.Post("/login", controllers.Login)
+}
