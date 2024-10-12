732845ad9d1289b5f51b3b826253758cbfd6b9ed
carrotbat410 <carrotbat410@gmail.com>
2024-10-12 15:48:23 +0900

    User Struct 수정

--- models/user.go
+++ models/user.go
@@ -1,12 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // User 구조체 정의
 type User struct {
-	ID        string    `json:"id,omitempty" bson:"_id,omitempty"`
-	Username  string    `json:"username" bson:"username"`
-	Email     string    `json:"email" bson:"email"`
-	Password  string    `json:"password" bson:"password"`
-	CreatedAt time.Time `bson:"created_at"`
+	UserID    primitive.ObjectID `json:"user_id,omitempty" bson:"_id"`
+	Id        string             `json:"id" bson:"id"`
+	Username  string             `json:"username" bson:"username"`
+	Password  string             `json:"password" bson:"password"`
+	CreatedAt time.Time          `bson:"created_at"`
 }
--- services/user.go
+++ services/user.go
@@ -15,7 +15,7 @@ import (
 // 유저 등록 서비스
 func RegisterUser(user models.User) error {
 	// 유효성 검사
-	if user.Username == "" || user.Email == "" || user.Password == "" {
+	if user.Id == "" || user.Username == "" || user.Password == "" {
 		return errors.New("All fields are required")
 	}
 
@@ -23,7 +23,7 @@ func RegisterUser(user models.User) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	count, err := database.UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+	count, err := database.UserCollection.CountDocuments(ctx, bson.M{"id": user.Id})
 	if err != nil {
 		return errors.New("Database error")
 	}
